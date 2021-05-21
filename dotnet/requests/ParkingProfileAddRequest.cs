fc94e9eb2014ec45af20cce53f9fdf755be93c93
MikeMiklos <77590227+MikeMiklos@users.noreply.github.com>
2021-05-21 14:42:04 -0700

    Update ParkingProfileAddRequest.cs

--- dotnet/requests/ParkingProfileAddRequest.cs
+++ dotnet/requests/ParkingProfileAddRequest.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 
-    public class ParkingProfileAddRequest
-    {
-        [Required, Range(1, Int32.MaxValue)]
-        public int LanguageId { get; set; }
-        
-        public int LocationId { get; set; }
-        
-        public LocationAddRequest Location { get; set; }
-        
-        [Required, Range(1, Int32.MaxValue)]
-        public int ParkingTypeId { get; set; }
-        
-        public string Description { get; set; }
-        
-        [Required]
-        public bool IsPrivate { get; set; }
-    }
+public class ParkingProfileAddRequest
+{
+    [Required, Range(1, Int32.MaxValue)]
+    public int LanguageId { get; set; }
+
+    public int LocationId { get; set; }
+
+    public LocationAddRequest Location { get; set; }
+
+    [Required, Range(1, Int32.MaxValue)]
+    public int ParkingTypeId { get; set; }
+
+    public string Description { get; set; }
+
+    [Required]
+    public bool IsPrivate { get; set; }
+}
 
