ece80d0feb76477661f87e58f2be198a864ee654
MikeMiklos <77590227+MikeMiklos@users.noreply.github.com>
2021-05-21 14:43:42 -0700

    Update ListingReservationApiController.cs

--- dotnet/controllers/ListingReservationApiController.cs
+++ dotnet/controllers/ListingReservationApiController.cs
@@ -6,200 +6,197 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Sabio.Web.Api.Controllers
+[Route("api/reservations")]
+[ApiController]
+public class ListingReservationApiController : BaseApiController
 {
-    [Route("api/reservations")]
-    [ApiController]
-    public class ListingReservationApiController : BaseApiController
+    private IListingReservationService _service = null;
+    private IEmailService _emailService = null;
+    private IAuthenticationService<int> _authService = null;
+
+    public ListingReservationApiController(IListingReservationService service
+        , ILogger<ListingReservationApiController> logger
+        , IEmailService emailService
+        , IAuthenticationService<int> authService) : base(logger)
     {
-        private IListingReservationService _service = null;
-        private IEmailService _emailService = null;
-        private IAuthenticationService<int> _authService = null;
+        _service = service;
+        _emailService = emailService;
+        _authService = authService;
+    }
 
-        public ListingReservationApiController(IListingReservationService service
-            , ILogger<ListingReservationApiController> logger
-            , IEmailService emailService
-            , IAuthenticationService<int> authService) : base(logger)
+    [HttpGet("{id:int}")]
+    public ActionResult<ItemResponse<ListingReservation>> GetDetailsById(int id)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
         {
-            _service = service;
-            _emailService = emailService;
-            _authService = authService;
-        }
+            ListingReservation reservation = _service.GetDetails(id);
 
-        [HttpGet("{id:int}")]
-        public ActionResult<ItemResponse<ListingReservation>> GetDetailsById(int id)
+            if (reservation == null)
+            {
+                code = 404;
+                response = new ErrorResponse("Application resource not found.");
+            }
+            else
+            {
+                response = new ItemResponse<ListingReservation> { Item = reservation };
+            }
+        }
+        catch (Exception ex)
         {
-            int code = 200;
-            BaseResponse response = null;
-
-            try
-            {
-                ListingReservation reservation = _service.GetDetails(id);
-
-                if (reservation == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("Application resource not found.");
-                }
-                else
-                {
-                    response = new ItemResponse<ListingReservation> { Item = reservation };
-                }
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
-            }
-
-            return StatusCode(code, response);
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
         }
 
-        [HttpGet("created/{id:int}")]
-        public ActionResult<ItemResponse<Paged<ListingReservation>>> GetByCreatedBy(int id, int pageIndex, int pageSize)
+        return StatusCode(code, response);
+    }
+
+    [HttpGet("created/{id:int}")]
+    public ActionResult<ItemResponse<Paged<ListingReservation>>> GetByCreatedBy(int id, int pageIndex, int pageSize)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
         {
-            int code = 200;
-            BaseResponse response = null;
+            Paged<ListingReservation> page = _service.GetByCreatedBy(id, pageIndex, pageSize);
 
-            try
+            if (page == null)
             {
-                Paged<ListingReservation> page = _service.GetByCreatedBy(id, pageIndex, pageSize);
-
-                if (page == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("Application resource not found.");
-                }
-                else
-                {
-                    response = new ItemResponse<Paged<ListingReservation>> { Item = page };
-                }
+                code = 404;
+                response = new ErrorResponse("Application resource not found.");
             }
-            catch (Exception ex)
+            else
             {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
+                response = new ItemResponse<Paged<ListingReservation>> { Item = page };
             }
-
-            return StatusCode(code, response);
         }
-
-        [HttpGet("host")]
-        public ActionResult<ItemsResponse<ListingReservation>> GetByHostId()
+        catch (Exception ex)
         {
-            int code = 200;
-            BaseResponse response = null;
-            try
-            {
-                int hostId = _authService.GetCurrentUserId();
-                List<ListingReservation> reservations = _service.GetByHostId(hostId);
-                if (reservations == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("No reservations found.");
-                }
-                else
-                {
-                    response = new ItemsResponse<ListingReservation> { Items = reservations };
-                }
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse($"Reservations Error: {ex.Message}");
-                base.Logger.LogError(ex.ToString());
-            }
-            return StatusCode(code, response);
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
         }
 
-        [HttpGet("status/{statusId:int}")]
-        public ActionResult<ItemsResponse<Paged<ListingReservation>>> GetByStatus(int statusId, int pageIndex, int pageSize)
+        return StatusCode(code, response);
+    }
+
+    [HttpGet("host")]
+    public ActionResult<ItemsResponse<ListingReservation>> GetByHostId()
+    {
+        int code = 200;
+        BaseResponse response = null;
+        try
         {
-            int code = 200;
-            BaseResponse response = null;
-
-            try
+            int hostId = _authService.GetCurrentUserId();
+            List<ListingReservation> reservations = _service.GetByHostId(hostId);
+            if (reservations == null)
             {
-                Paged<ListingReservation> page = _service.GetByStatus(statusId, pageIndex, pageSize);
-
-                if (page == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("Resrvations not found.");
-                }
-                else
-                {
-                    response = new ItemResponse<Paged<ListingReservation>> { Item = page };
-                }
+                code = 404;
+                response = new ErrorResponse("No reservations found.");
             }
-            catch (Exception ex)
+            else
             {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
+                response = new ItemsResponse<ListingReservation> { Items = reservations };
             }
-
-            return StatusCode(code, response);
         }
-
-        [HttpGet("search")]
-        public ActionResult<ItemResponse<Paged<ListingReservation>>> SearchByFanHostListing(string query, int pageIndex, int pageSize)
+        catch (Exception ex)
         {
-            int code = 200;
-            BaseResponse response = null;
+            code = 500;
+            response = new ErrorResponse($"Reservations Error: {ex.Message}");
+            base.Logger.LogError(ex.ToString());
+        }
+        return StatusCode(code, response);
+    }
 
-            try
+    [HttpGet("status/{statusId:int}")]
+    public ActionResult<ItemsResponse<Paged<ListingReservation>>> GetByStatus(int statusId, int pageIndex, int pageSize)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            Paged<ListingReservation> page = _service.GetByStatus(statusId, pageIndex, pageSize);
+
+            if (page == null)
             {
-                Paged<ListingReservation> page = _service.SearchByFanHostListing(query, pageIndex, pageSize);
-
-                if (page == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("Application resource not found.");
-                }
-                else
-                {
-                    response = new ItemResponse<Paged<ListingReservation>> { Item = page };
-                }
+                code = 404;
+                response = new ErrorResponse("Resrvations not found.");
             }
-            catch (Exception ex)
+            else
             {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
+                response = new ItemResponse<Paged<ListingReservation>> { Item = page };
             }
-
-            return StatusCode(code, response);
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
         }
 
-        [HttpPut("oldRes")]
-        public ActionResult<ItemsResponse<ReservationCloseoutRequest>> UpdateOldReservations(List<int> reservationIdList)
-        {            
-            int code = 200;
-            BaseResponse response = null;
-            List<ReservationCloseoutRequest> resList = null;
-            try
-            {
-                resList = _service.UpdateOldReservations(reservationIdList);
-                if (resList != null)
-                {
-                    foreach (ReservationCloseoutRequest res in resList)
-                    {
-                        _emailService.SendReservationCloseoutEmail(res);
-                    }
-                }
-                response = new ItemsResponse<ReservationCloseoutRequest> { Items = resList };
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                base.Logger.LogError(ex.ToString());
-                response = new ErrorResponse($"Error updating old reservations: {ex.Message}");
-            }
+        return StatusCode(code, response);
+    }
 
-            return StatusCode(code, response);
+    [HttpGet("search")]
+    public ActionResult<ItemResponse<Paged<ListingReservation>>> SearchByFanHostListing(string query, int pageIndex, int pageSize)
+    {
+        int code = 200;
+        BaseResponse response = null;
+
+        try
+        {
+            Paged<ListingReservation> page = _service.SearchByFanHostListing(query, pageIndex, pageSize);
+
+            if (page == null)
+            {
+                code = 404;
+                response = new ErrorResponse("Application resource not found.");
+            }
+            else
+            {
+                response = new ItemResponse<Paged<ListingReservation>> { Item = page };
+            }
         }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+            base.Logger.LogError(ex.ToString());
+        }
+
+        return StatusCode(code, response);
+    }
+
+    [HttpPut("oldRes")]
+    public ActionResult<ItemsResponse<ReservationCloseoutRequest>> UpdateOldReservations(List<int> reservationIdList)
+    {            
+        int code = 200;
+        BaseResponse response = null;
+        List<ReservationCloseoutRequest> resList = null;
+        try
+        {
+            resList = _service.UpdateOldReservations(reservationIdList);
+            if (resList != null)
+            {
+                foreach (ReservationCloseoutRequest res in resList)
+                {
+                    _emailService.SendReservationCloseoutEmail(res);
+                }
+            }
+            response = new ItemsResponse<ReservationCloseoutRequest> { Items = resList };
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            base.Logger.LogError(ex.ToString());
+            response = new ErrorResponse($"Error updating old reservations: {ex.Message}");
+        }
+
+        return StatusCode(code, response);
     }
 }
