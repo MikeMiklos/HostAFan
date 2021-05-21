cdbee0feede78f03611900cf5a03cab642f89961
MikeMiklos <77590227+MikeMiklos@users.noreply.github.com>
2021-05-21 14:39:32 -0700

    Update ListingReservationApiController.cs

--- dotnet/controllers/ListingReservationApiController.cs
+++ dotnet/controllers/ListingReservationApiController.cs
@@ -1,14 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Sabio.Models;
-using Sabio.Models.Domain;
-using Sabio.Models.Requests;
-using Sabio.Models.Requests.ListingReservations;
-using Sabio.Services;
-using Sabio.Services.Interfaces;
-using Sabio.Web.Controllers;
-using Sabio.Web.Models.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,36 +24,6 @@ namespace Sabio.Web.Api.Controllers
             _service = service;
             _emailService = emailService;
             _authService = authService;
-        }
-
-        [HttpGet]
-        public ActionResult<ItemResponse<Paged<ListingReservationBase>>> GetAll(int pageIndex, int pageSize)
-        {
-            int code = 200;
-            BaseResponse response = null;
-
-            try
-            {
-                Paged<ListingReservationBase> page = _service.GetAll(pageIndex, pageSize);
-                if (page == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("Application resource not found.");
-                }
-                else
-                {
-                    response = new ItemResponse<Paged<ListingReservationBase>> { Item = page };
-                }
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-                base.Logger.LogError(ex.ToString());
-            }
-
-            return StatusCode(code, response);
-
         }
 
         [HttpGet("{id:int}")]
@@ -207,45 +169,6 @@ namespace Sabio.Web.Api.Controllers
                 code = 500;
                 response = new ErrorResponse(ex.Message);
                 base.Logger.LogError(ex.ToString());
-            }
-
-            return StatusCode(code, response);
-        }
-
-        //[HttpPost]
-        //public ActionResult<ItemResponse<int>> Create(ListingReservationAddRequest model)
-        //{
-        //    ObjectResult result = null;
-        //    try
-        //    {
-        //        int userId = _authService.GetCurrentUserId();
-        //        int id = _service.Add(model, userId);
-        //        ItemResponse<int> response = new ItemResponse<int>() { Item = id };
-        //        result = Created201(response);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Logger.LogError(ex.ToString());
-        //        ErrorResponse response = new ErrorResponse(ex.Message);
-        //        result = StatusCode(500, response);
-        //    }
-        //    return result;
-        //}
-        [HttpPut("{id:int}")]
-        public ActionResult<SuccessResponse> Update(ListingReservationUpdateRequest model)
-        {
-            int code = 200;
-            BaseResponse response = null;
-
-            try
-            {
-                _service.Update(model);
-                response = new SuccessResponse();
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
             }
 
             return StatusCode(code, response);
