9d1998d9cef2017d9a97ef8cea1032f4e0c4969c
MikeMiklos <77590227+MikeMiklos@users.noreply.github.com>
2021-05-21 14:41:20 -0700

    Update ParkingProfileAddRequest.cs

--- dotnet/requests/ParkingProfileAddRequest.cs
+++ dotnet/requests/ParkingProfileAddRequest.cs
@@ -1,21 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
-using Sabio.Models.Requests.Locations;
 
-namespace Sabio.Models.Requests.ParkingProfiles
-{
     public class ParkingProfileAddRequest
     {
         [Required, Range(1, Int32.MaxValue)]
         public int LanguageId { get; set; }
+        
         public int LocationId { get; set; }
+        
         public LocationAddRequest Location { get; set; }
+        
         [Required, Range(1, Int32.MaxValue)]
         public int ParkingTypeId { get; set; }
+        
         public string Description { get; set; }
+        
         [Required]
         public bool IsPrivate { get; set; }
     }
-}
+
